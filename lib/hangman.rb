c992f2a1eb0051a0880a054e05c98cf8b7c338f2
rmassey95 <rmassey.95@gmail.com>
2022-03-22 18:27:08 -0400

    Add basic letter guessing functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -10,10 +10,56 @@ word_file = File.open('google-10000-english-no-swears.txt', 'r')
 
 all_words = word_file.readlines
 good_word = false
+trys = 1
+win = false
 
+guesses = Hash.new
 until good_word do
   word = all_words[Random.rand(0..10000)].chomp
   good_word = check_word(word)
+end
+
+puts word
+
+display = "_"*word.length
+
+while trys <= 8 do
+
+  puts "Enter a letter: "
+  guess = gets.chomp
+
+  if word.include?(guess)
+    word.split('').each_index do |index|
+      if word[index] == guess
+        display[index] = guess
+        if guesses[guess] == nil
+          guesses[guess] = 1
+        else
+          guesses[guess] += 1
+        end
+      end
+    end
+  else
+    guesses[guess] = 0
+  end
+
+  if guesses.values.sum == word.length
+    puts "WIN"
+    win = true
+    break
+  else
+    puts "NOT"
+  end
+  puts display
+
+  trys += 1
+
+end
+
+if win
+  puts "YOU GOT THE WORD! IT WAS #{word}"
+else
+  puts "YOU DIDN'T GET THE WORD"
 end
 
 word_file.close
