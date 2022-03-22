9f6d0eadc31850b8929815bfa4f38632d46b7972
rmassey95 <rmassey.95@gmail.com>
2022-03-22 17:56:00 -0400

    Add function to choose random word from word list

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,9 +1,19 @@
+def check_word(word)
+  if word.length < 5 || word.length > 12
+    return false
+  else
+    return true
+  end
+end
+
 word_file = File.open('google-10000-english-no-swears.txt', 'r')
 
 all_words = word_file.readlines
+good_word = false
 
-puts all_words[Random.rand(0..10000)]
-
-
+until good_word do
+  word = all_words[Random.rand(0..10000)].chomp
+  good_word = check_word(word)
+end
 
 word_file.close
